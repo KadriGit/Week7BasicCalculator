a896e4582accda4e32fbffda227ef7b562b8ed02
KadriGit <kadri.viira@tktk.ee>
2024-11-04 21:08:13 +0200

    final

--- Week7BasicCalculator/Program.cs
+++ Week7BasicCalculator/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 20;
+int b = 4;
+
+Add(a, b);
+Add(10, 100);
+Add(56583, 658686);
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(56583, 658686);
+
+
+Substract(a, b);
+Divide(a, b);;
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x+y}");
+}
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+static void Substract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
+
+static void Divide(int x, int y)
+{
+    Console.WriteLine($"{x} / {y} = {x / y}");
+}
+
